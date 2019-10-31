14d95a91b87f85708d2405eed63c01d16ed12d27
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-10-31 10:12:05 -0700

    h

--- sketch.js
+++ sketch.js
@@ -1,33 +1,37 @@
-let input, button, greeting;
-
+// let input, button, greeting;
+//
+// function setup() {
+//   noCanvas();
+//
+//   input = createInput();
+//   input.position(65, 65);
+//
+//   button = createButton('say something');
+//   button.position(input.x + input.width, 65);
+//   button.mousePressed(greet);
+//
+//   greeting = createElement('h2', 'meow?');
+//   greeting.position(65, 5);
+//
+//   textAlign(CENTER);
+//   textSize(50);
+// }
+//
+// function greet() {
+//   const message = input.value();
+//   greeting.html('meow ' + name + '!');
+//   input.value('');
+// }
 function setup() {
-  createCanvas(600, 600);
-
-  input = createInput();
-  input.position(65, 65);
-
-  button = createButton('say something');
-  button.position(input.x + input.width, 65);
-  button.mousePressed(greet);
-
-  greeting = createElement('h2', 'meow?');
-  greeting.position(65, 5);
-
-  textAlign(CENTER);
-  textSize(50);
+  noCanvas();
+}
+function draw() {
+  background(220);
 }
 
-function greet() {
-  const name = input.value();
-  greeting.html('meow ' + name + '!');
-  input.value('');
-
-  for (let i = 0; i < 200; i++) {
-    push();
-    fill(random(255), 255, 255);
-    translate(random(width), random(height));
-    rotate(random(2 * PI));
-    text(name, 0, 0);
-    pop();
-  }
+function getTime() {
+  let h = hour();
+  let m = minute();
+  let ampm = h <= 12 ? "AM" : "PM"
+  return "" + h % 12 + ":" + m + " " + ampm
 }
