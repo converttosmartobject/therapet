f1a5037fa8d18d4fd32984070dd48ad9c4ce255e
Asra Siddiqui <asrasiddiqui127@gmail.com>
2019-11-04 18:05:54 -0800

    all

--- sketch.js
+++ sketch.js
@@ -1,53 +1,50 @@
-// chatArea is the variable that stores the place on the screen the chats will appear.
+// place on the screen the chats will appear
 var chatArea = document.getElementById('chat-area');
 
-// count is a variable that stores how many total chats have been sent.
+// how many total chats have been sent
 var count = 0;
 
-// botCount and userCount are variables that store how many chats each the bot and the user have sent.
+// how many chats each the bot and the user have sent
 var botCount = 0;
 var userCount = 0;
 
-// nextMessage is an object variable that stores the next message that will be sent and who will be sending it.
+// next message that will be sent and who will be sending it
 var nextMessage = {
   message: "",
   sender: ""
 };
 
-// botSilent is a variable that stores when the bot is done speaking because it has said all of the things in the script.
+// when the bot is done speaking because it has said all of the things in the script
 var botSilent = false;
 
-// botChat is the function that picks the bot's next message.
-
+// picks the bot's next message.
 function botChat() {
-  // Set the bot as the sender of the next message.
+  // set the bot as the sender of the next message
   nextMessage.sender = "bot";
 
   if (botCount >= botScript.length) {
     nextMessage.message = goodbye
     botSilent = true;
   } else {
-    // Set the bot's next message as the next string in the botScript array.
+    // set the bot's next message as the next string in the botScript array
     nextMessage.message = botScript[botCount];
   }
 
-  // Send the bot's message.
+  // send the bot's message
   send(nextMessage.sender, nextMessage.message);
 
-//   // Count 1 more chat that the bot has sent
-
+   // count 1 more chat that the bot has sent
   botCount += 1;
 
-  // Start listening again after the bot has sent a message.
+  // start listening again after the bot has sent a message
   listenFor();
 }
 
 function lookForChat() {
-  // If there have been no chats yet, start the bot.
+  // if there have been no chats yet, start the bot
   if (count == 0) {
     startBot();
   }
-
 
   // check who sent the last chat
   last = nextMessage.sender;
@@ -57,12 +54,12 @@ function lookForChat() {
     userChat();
 
   } else {
-    // Send the cursor to the compose text area.
+    // send the cursor to the compose text area
     composer.focus();
 
-    // If botSilent is true the bot is done chatting
-    // Set nextMessage.sender to "bot" to make the user chat next
-    // Run listenFor() to wait for the user to chat.
+    // if botSilent is true the bot is done chatting
+    // set nextMessage.sender to "bot" to make the user chat next
+    // run listenFor() to wait for the user to chat
     if (botSilent) {
       nextMessage.sender = "bot";
       nextMessage.message = "";
@@ -70,9 +67,9 @@ function lookForChat() {
 
     } else {
 
-      // If the user chatted last or the chat just started have the bot send a chat.
-      // Set the appropriate wait time to make the bot feel realistic.
-      // Then run the botChat function which will find the right message for the bot
+      // if the user chatted last or the chat just started have the bot send a chat
+      // set the appropriate wait time to make the bot feel realistic
+      // then run the botChat function which will find the right message for the bot
       if (count == 0) {
         wait = 100;
       } else {
@@ -85,8 +82,7 @@ function lookForChat() {
   }
 }
 
-
-// startBot is a function that starts the bot for the first time. It clears away the start button from the HTML.
+// starts the bot for the first time and clears away the start button from the HTML
 function startBot(){
   chatArea.innerHTML = '';
   document.getElementById('compose-area').style.display = 'block';
@@ -133,7 +129,6 @@ function pauseListening() {
 }
 
 // send is the function that sends the next message stored in nextMessage object.
-
 
 function send(sender, message) {
 
